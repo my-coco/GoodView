4f6bb82f692d6b284842af2b792816e6ba357361
my-coco <3386422064@qq.com>
2022-02-09 16:27:44 +0800

    创建项目

---
+++ app/src/main/java/com/lisixing/ProBarView/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lisixing.ProBarView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lisixing.ProBarView.widget.RectProBarView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private RectProBarView rectProBarView;
+    private float progress=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        rectProBarView=findViewById(R.id.rectProBarView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress+=10;
+                rectProBarView.setProgress(progress);
+            }
+        });
+    }
+}
